f7d783c44592d00cc3f9f52ea39861b7e2972865
Deepender Singh <deepenrathore28@gmail.com>
2025-05-15 18:13:04 +0200

    Adding script to check Quality jobs and print number of jobs

---
+++ tests/careers.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('Careers page should list Quality ', async ({ page }) => {
+  await page.goto('https://careers.osapiens.com/');
+
+  const jobListings = await page.getByRole('rowgroup').all();
+
+  const numberOfJobListings = await jobListings.length;
+
+  console.log(`numberOfJobListings: ${numberOfJobListings}`);
+
+  const jobTitles: string[] = [];
+
+  for (const row of jobListings) {
+    const titleElement = await row.getByRole('gridcell').locator('a').first();
+
+    const title = await titleElement.textContent();
+    if (title) jobTitles.push(title.trim());
+  }
+
+  const qualityJobs = jobTitles.filter((title) => title.includes('Quality'));
+
+  expect(qualityJobs.length, {
+    message: 'Expected at least one job title to contain "Quality"',
+  }).toBeGreaterThan(0);
+});
